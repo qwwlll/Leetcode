7d3dbe15da947e1e0239e1a108d8a10813299ccf
qwwlll <83490554+qwwlll@users.noreply.github.com>
2021-06-09 14:40:28 +0800

    leetcode02-Sum of two numbers

---
+++ leetcode02.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = ListNode(0)
+        r = result
+        carry = 0
+        while(l1 or l2):
+            x= l1.val if l1 else 0
+            y= l2.val if l2 else 0
+            s=carry+x+y
+            carry=s//10
+            r.next=ListNode(s%10)
+            r=r.next
+            if(l1!=None):l1=l1.next
+            if(l2!=None):l2=l2.next
+        if(carry>0):
+            r.next=ListNode(1)
+        return re.next
