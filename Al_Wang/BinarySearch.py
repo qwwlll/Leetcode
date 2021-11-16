51525e0d3353ab9c5b9eab3d3c7caafc41ca173e
乔玮伦 <u6252098@anu.edu.au>
2021-11-16 18:17:36 +0800

    HK,BS

--- Al_Wang/BinarySearch.py
+++ Al_Wang/BinarySearch.py
@@ -14,6 +14,7 @@ nums = [1,3,4,5,6,8,8,8,11,13]
 print(firstEle(nums,8))
 ### other way, by using the binary search
 ### O(logn):
+### leetcode 704 二分查找n
 def firstEleBS (nums: List[int], target : int) -> int: 
     low = 0
     high = len(nums) - 1
@@ -27,7 +28,7 @@ def firstEleBS (nums: List[int], target : int) -> int:
     if (low < len(nums) and nums[low] == target): return low
     else: return -1
 print(firstEleBS(nums,8))
-### otehr way of BS
+### otehr way of B
 def firstEleBS1 (nums: List[int], target : int) -> int: 
     low = 0
     high = len(nums)-1
@@ -204,4 +205,82 @@ class Solution:
         sam = high + 1
         return [sam, big]
         
+#### leetcode 35 搜索插入位置
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if target > nums[n-1]:
+            return n
+        low, high = 0, n-1
+        while low <= high:
+            mid = low + ((high- low )>>1)
+            if nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                return mid
+        return low
 
+### leetcode 852 寻找峰顶
+### 二分查找
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        if len(arr) == 3:
+            return 1        
+        low, high = 0, len(arr)-1
+        while low <= high:
+            mid = low + ((high - low ) >>1 )
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+            elif arr[mid]> arr[mid+1] and arr[mid] < arr[mid-1]:
+                high = mid - 1
+            elif arr[mid]< arr[mid+1] and arr[mid] > arr[mid-1]:
+                low = mid + 1
+        return low
+
+### leetcode 658 找k个最接近的数
+### 闭区间二分
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        left = 0
+        right = len(arr) - k - 1
+        while (left <= right) :
+            mid = left + (right - left) // 2
+            if (x - arr[mid] > arr[mid + k] - x) :
+                left = mid + 1
+            else :
+                right = mid - 1
+        return arr[left : left + k]
+
+### leetcode 69 squt
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return 1
+        mi, mx, res = 0, x, 0
+        while mi <= mx:
+            mid = (mi + mx) //2
+            if mid * mid <= x:
+                res = mid
+                mi = mid + 1
+            else:
+                mx = mid - 1
+        return res
+    
+### leetcode 287寻找重复的数
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        left = 1
+        right = len(nums)
+        while left < right:
+            mid = left + ((right - left)>>1)
+            cnt = 0
+            for num in nums:
+                if num <= mid:
+                   cnt += 1
+            if cnt <= mid:
+                left = mid + 1
+            else:
+                right = mid
+        return right
