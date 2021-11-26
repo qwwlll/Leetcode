bec13d5e6f86d3b60f9aae9bb0a72870480cc705
乔玮伦 <u6252098@anu.edu.au>
2021-11-26 16:31:14 +0800

    un updated the other day

--- Al_Wang/Stack.py
+++ Al_Wang/Stack.py
@@ -75,7 +75,131 @@ class Solution:
 
         return de(s) == de(t)
 
-##leetcode 224
+##leetcode 224 简单计算器
+    def solve(self, s):
+        # write code here
+        def eva(s):
+            stack = []
+            num = 0 
+            sign = "+"
+            while len(s) > 0 :
+                x = s.pop(0)
+                if x.isdigit():
+                    num = num * 10 + int(x)
+                if x == "(":
+                    num = eva(s)
+                if ((not x.isdigit()) and x != " ") or len(s) == 0 :
+                    if sign == "+":
+                        stack.append(num)
+                    elif sign == "-":
+                        stack.append(-num)
+                    num = 0 
+                    sign = x
+                if x == ")":
+                    break
+            return sum(stack)
+        return eva(list(s))
+
+### edited with * and /
+class Solution:
+    def solve(self, s):
+        # write code here
+        def eva(s):
+            stack = []
+            num = 0 
+            sign = "+"
+            while len(s) > 0 :
+                x = s.pop(0)
+                if x.isdigit():
+                    num = num * 10 + int(x)
+                if x == "(":
+                    num = eva(s)
+                if ((not x.isdigit()) and x != " ") or len(s) == 0 :
+                    if sign == "+":
+                        stack.append(num)
+                    elif sign == "-":
+                        stack.append(-num)
+                    elif sign == "*":
+                        temp = stack.pop()
+                        stack.append(temp * num)
+                    elif sign =="/":
+                        temp = stack.pop()
+                        stack.append(temp/float(num))
+                    num = 0 
+                    sign = x
+                if x == ")":
+                    break
+            return sum(stack)
+        return eva(list(s))
+                    
+
 ##leetcode 232
+##用双栈实现队列
+class MyQueue:
+    
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+       
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+
+### leetcode 225
+### 用双队列来表示栈
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q1.append(x)
+        while self.q2:
+            self.q1.append(self.q2.popleft())
+        self.q1, self.q2 = self.q2, self.q1
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q2.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q2[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not self.q2
+
 ##leetcode 682
 ##leetcode 496
