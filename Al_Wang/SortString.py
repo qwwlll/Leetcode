5cbef655700bfdc7297742de89e63c026f955c9e
乔玮伦 <u6252098@anu.edu.au>
2021-11-29 21:41:52 +0800

    Tryout String

---
+++ Al_Wang/SortString.py
@@ -0,0 +1,24 @@
+from typing import Counter, List
+### 给string 进行不同需求的排序
+def sortingStr(n: List[str]) -> List[str]:
+### 将list n 先以长度后以字符序排序
+
+#    n.sort(key= lambda i :(len(i), i))
+
+### 将list n 先以字符序排序后以长度
+#    n.sort(key = lambda i:(i, len(i))) ### 先后顺序matters！
+
+### 先将list n 以 a 的重复次数排序 后以长度后以字符序排序
+    dic = {}
+    def counting(x):
+        return x.count('a')
+
+###    result = sorted(dic.items(), key= lambda x: x[1],reverse= True)
+    res = sorted(n, key = counting,reverse= True)
+    return res
+    
+
+
+
+n = ['asdas', 'asfw', 'efa', 'hfeafa', 'ddefaf','abc','ac','aacs','abbcs']
+print(sortingStr(n))
