0c0967614a277c96133429f84ca05700b722154b
乔玮伦 <u6252098@anu.edu.au>
2021-12-27 14:23:38 +0800

    leetcoding

--- Al_Wang/ArrayList.py
+++ Al_Wang/ArrayList.py
@@ -189,4 +189,54 @@ while True:
         print(def_huiwen(a))
     except:
         break
+
+
+### leetcode 134 加油站
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        total, start = 0,0
+        if sum(gas) < sum(cost):
+            return -1
+        for i in range(n):
+            total += gas[i] - cost[i]
+            if total <0 :
+                total = 0
+                start = i + 1
+        return start
+
+
+### leetcode 130 被围绕的区域(dfs)
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return 
+        n, m = len(board), len(board[0])
+
+        def dfs(x, y):
+            if not 0 <= x < n or not 0 <= y < m or board[x][y] != "O":
+                return 
+            board[x][y] = "!"
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
+
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m - 1)
+        
+        for i in range(m - 1):
+            dfs(0, i)
+            dfs(n - 1, i)
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "!":
+                    board[i][j] ="O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
             
