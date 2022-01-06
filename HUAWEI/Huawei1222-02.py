95ac18d2bd87136dc72766a8c34dc02645ba2bf0
乔玮伦 <u6252098@anu.edu.au>
2022-01-06 14:55:35 +0800

    Huawei Q3

--- HUAWEI/Huawei1222-02.py
+++ HUAWEI/Huawei1222-02.py
@@ -1,8 +1,24 @@
 from typing import List
 #### 列车订票系统
-while 1:
-    try:
-        a, b, lt = int(input()), int(input()), input()
+# while 1:
+#     try:
+#         a, b, lt = int(input()), int(input()), input()
 
-    except:
-        break
+#     except:
+#         break
+
+# a, b, stops = int(input()), int(input()), input()
+
+def oreder(a: int, b: int, stops: List[List[int]]):
+    res = []
+    #a = stop_num
+    #b = seat_num
+    stops1 = stops.sort(key = lambda i : int(stops[i][1]))
+    return print(stops1)
+
+
+
+a = 10
+b = 100
+stops = [[0,5],[0,9],[6,10]]
+oreder(a, b, stops)
--- HUAWEI/Huawei1222-03.py
+++ HUAWEI/Huawei1222-03.py
@@ -15,11 +15,42 @@ def min_distance(a:List[List[int]]):
     return dp[-1][-1]
 
 
-a = [[1,3,5,9],[8,1,3,4],[5,0,6,1],[8,8,4,0]]
-print(min_distance(a))
+
+# import sys
+# def min_distance(a:List[List[int]]):
+#     m, n = len(a), len(a[0])
+#     dp = [[0] * n for _ in range(m)]
+#     dp[0][0] = a[0][0]
+#     for i in range ( m):
+#         for j in range( n):
+#             if i == 0 and j != 0:
+#                 dp[i][j] = dp[i][j-1] + a[i][j]
+#             elif j == 0 and i != 0:
+#                 dp[i][j] = dp[i-1][j] + a[i][j]
+#             else:
+#                 dp[i][j] = a[i][j] + min(dp[i-1][j],dp[i][j-1])
+#     return print(dp[-1][-1])
+
+# while 1:
+#     try:
+#         list = []  
+#         for line in sys.stdin:      
+#             res =[]
+#             for li in line.split(","):
+#                 res.append(int(li))
+#             list.append(res) 
+#         min_distance(list)
+#     except:
+#         break
 
 
 
+# 10,30,30,20
+# 20,30,30,30
+# 30,20,20,10
+# 30,40,50,30
+# 10,20
+# 30,40
 #### 加油站问题
 def min_distance1(a:List[List[int]]):
     
