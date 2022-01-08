24be87dbfab0f350d82850d2e098a3b4eb719140
乔玮伦 <u6252098@anu.edu.au>
2022-01-08 16:07:09 +0800

    interview

--- Al_Wang/BinaryTree.py
+++ Al_Wang/BinaryTree.py
@@ -368,3 +368,33 @@ class Solution:
             return self.searchBST(root.right, val)
 
 
+
+### leetcode 582 杀进程
+from typing import DefaultDict, List
+def kill_jc(pid: List[int], ppid: List[int],kill: int):
+    n = len(ppid)
+    dic = DefaultDict(list)
+    for i in range(n):
+        dic[ppid[i]].append(pid[i])
+    
+    stack = []
+    end = []
+    stack.append(kill)
+    while stack:
+        kb = stack[-1]
+        end.append(kb)
+        stack.pop()
+        if dic[kb]:
+            stack = stack + dic[kb]
+            dic[kb] = []
+    return end
+
+
+
+
+pid = [1,3,10,5] 
+ppid = [3,0,5,3]
+kill = 5
+print(kill_jc( pid,ppid, kill))
+
+
